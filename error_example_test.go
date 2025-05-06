3a412848fc97b00bc8e4fcff1800dd9cbe6a518d
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-05-06 15:44:09 +0200

    Remove example to fix CI

--- error_example_test.go
+++ error_example_test.go
@@ -5,23 +5,6 @@ import (
 	"fmt"
 )
 
-func ExampleErr_JSON() {
-	err := Err{
-		Value:     errors.New("test"),
-		Msg:       "My error message",
-		Details:   nil,
-		File:      "error_test.go",
-		Line:      26,
-		Timestamp: 1691234567890,
-		Prev:      nil,
-	}
-	result, _ := err.JSON()
-
-	fmt.Println(string(result))
-
-	// Output: {"value":"test","details":null,"timestamp":"1970-01-20T14:47:14.56789+01:00","msg":"My error message","file":"error_test.go","line":26,"prev":null}
-}
-
 func ExampleErr_Is() {
 	var myErr = errors.New("my error")
 	err := Err{
